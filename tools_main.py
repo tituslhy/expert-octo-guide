b1bf3a470870d87e20c73e00b711276008091dbc
Titus Lim <tituslhy@gmail.com>
2025-04-17 15:13:13 +0800

    code edit

--- tools_main.py
+++ tools_main.py
@@ -1,12 +1,14 @@
 from fastmcp import FastMCP
 from mcp.stock_tools import mcp as stock_mcp
 from mcp.writing_tools import mcp as writing_mcp
+from mcp.weather_tools import mcp as weather_mcp
 
 mcp = FastMCP("Writing and stock analysis tools")
 
 mcp.mount(
     "stock_tools", stock_mcp,
-    "writing_tools", writing_mcp
+    "writing_tools", writing_mcp,
+    "weather_tools", weather_mcp
 )
 
 @mcp.tool()
