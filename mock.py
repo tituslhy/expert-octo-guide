8df21c982383618481f98c0b74bb8ac8660f8d28
Titus Lim <tituslhy@gmail.com>
2025-04-19 14:34:11 +0800

    code update

--- mock.py
+++ mock.py
@@ -1,20 +1,19 @@
 import chainlit as cl
 
 from llama_index.core.agent import FunctionCallingAgent
-from llama_index.core.base.llms.types import ChatMessage
+from llama_index.core.base.llms.types import ChatMessage, MessageRole
 from llama_index.core.chat_engine import SimpleChatEngine
 from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.llms.ollama import Ollama
 from llama_index.tools.mcp import BasicMCPClient, McpToolSpec
 
-from mcp import ClientSession
-from mcp.types import CallToolResult, TextContent
-
 llm = Ollama(model="qwen2.5", temperature=0)
-mcp_tools_cache = {}
 
 @cl.on_chat_start
 async def start():
+    cl.user_session.set(
+        "mcp_tools", {}
+    )
     cl.user_session.set(
         "agent",
         SimpleChatEngine.from_defaults(
@@ -24,7 +23,7 @@ async def start():
     memory = ChatMemoryBuffer.from_defaults()
     memory.put(
         ChatMessage(
-            role="system", 
+            role=MessageRole.SYSTEM, 
             content="You are a helpful AI assistant. You can access tools using MCP servers if available."
         )
     )
@@ -33,58 +32,72 @@ async def start():
         memory,
     )
 
+@cl.on_message
+async def on_message(message: cl.Message):
+    memory = cl.user_session.get("memory")
+    # Retrieve the chat history
+    chat_history = memory.get()
+    agent = cl.user_session.get("agent")
+    response = await agent.achat(message.content, chat_history=chat_history)
+    memory.put(
+        ChatMessage(
+            role=MessageRole.USER,
+            content=message.content,
+        )
+    )
+    memory.put(
+        ChatMessage(
+            role=MessageRole.ASSISTANT,
+            content=str(response),
+        )
+    )
+    cl.user_session.set("memory", memory)
+    await cl.Message(content=str(response)).send()
+
 @cl.on_mcp_connect
-async def on_mcp_connect(
-    connection, 
-    session: ClientSession
-):
-    
+async def on_mcp_connect(connection):
+    mcp_tools = cl.user_session.get("mcp_tools", {})
     try:
         mcp_client = BasicMCPClient(connection.url)
         mcp_tool_spec = McpToolSpec(client=mcp_client)
-        tools = await mcp_tool_spec.to_tool_list_asnyc()
+        new_tools = await mcp_tool_spec.to_tool_list_async()
+        for tool in new_tools:
+            if tool.metadata.name not in mcp_tools:
+                mcp_tools[tool.metadata.name] = tool
         agent = cl.user_session.get("agent")
         if isinstance(agent, SimpleChatEngine):
             agent = FunctionCallingAgent.from_tools(
-                llm = llm,
-                tools = tools
+                tools=list(mcp_tools.values()),
+                llm=llm,
             )
-        else: #update tool list
-            ...
+        
         cl.user_session.set("agent", agent)
-        await cl.Message(f"Connected to MCP server: {connection.name} on {connection.url}").send()
-    
-        result = await session.list_tools()
-
-        tools = [
-            {
-                "name": t.name,
-                "description": t.description,
-                "input_schema": t.inputSchema,
-            }
-            for t in result.tools
-        ]
-
-        mcp_tools_cache[connection.name] = tools
-
-        mcp_tools = cl.user_session.get("mcp_tools", {})
-        mcp_tools[connection.name] = tools
         cl.user_session.set("mcp_tools", mcp_tools)
+        await cl.Message(f"Connected to MCP server: {connection.name} on {connection.url}").send()
 
         await cl.Message(
-            f"Found {len(tools)} tools from {connection.name} MCP server."
+            f"Found {len(new_tools)} tools from {connection.name} MCP server."
         ).send()
     except Exception as e:
         await cl.Message(f"Error listing tools from MCP server: {str(e)}").send()
 
 @cl.on_mcp_disconnect
-async def on_mcp_disconnect(name: str, session: ClientSession):
-    if name in mcp_tools_cache:
-        del mcp_tools_cache[name]
-
+async def on_mcp_disconnect(name: str):
     mcp_tools = cl.user_session.get("mcp_tools", {})
     if name in mcp_tools:
         del mcp_tools[name]
-        cl.user_session.set("mcp_tools", mcp_tools)
 
+    # Update tools list in agent
+    if len(mcp_tools)>0:
+        agent = FunctionCallingAgent.from_tools(
+            tools=list(mcp_tools.values()), #agent still has tools not removed
+            llm=llm,
+        )
+    else:
+        agent = SimpleChatEngine.from_defaults(
+            llm=llm,
+        )
+    cl.user_session.set("mcp_tools", mcp_tools)
+    cl.user_session.set("agent", agent)
+    
     await cl.Message(f"Disconnected from MCP server: {name}").send()
